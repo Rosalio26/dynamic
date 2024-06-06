ba521f1c39ae532cc69030afbe3ea360765170e2
Rosalio26 <rosalioalexandre26@gmail.com>
2024-06-06 22:04:50 +0200

    ch

--- js/miniconf.js
+++ js/miniconf.js
@@ -1,11 +1,33 @@
+
+function slideup(){
+    var elem = document.querySelector('.its-content');
+    var ell = document.getElementById("show");
+    var hidde = document.getElementById("hidden");
+
+    elem.style.display="block";
+    ell.style.display = "none";
+    hidde.style.display = "block"
+};
+
+function slidedown(){
+    var elem = document.querySelector('.its-content');
+    var ell = document.getElementById("show");
+    var hidde = document.getElementById("hidden");
+
+    elem.style.display="none";
+    ell.style.display = "block";
+    hidde.style.display = "none";
+};
+
 //Definindo as variaveis dos botoes
-var btnVisibley = document.getElementById('btn-hamb');
+/*var btnVisibley = document.getElementById('btn-hamb');
 var btnHidden = document.getElementById('btn-hamb-1');
 var contEnt = document.querySelector('.content-itm');
 var contentBone = document.querySelector('#esp-itm');
+const screns = window.matchMedia('(max-width: 680px)');
 
-btnVisibley.addEventListener('click', clicOne);
-btnHidden.addEventListener('click', clicTwo);
+    btnVisibley.addEventListener('click', clicOne);
+    btnHidden.addEventListener('click', clicTwo);
 
 function clicOne() {
     btnVisibley.style.display = "none";
@@ -19,4 +41,4 @@ function clicTwo() {
     btnHidden.style.display = "none";
     contEnt.style.display = "none";
     contentBone.style.display = "block";
-}
+}*/
