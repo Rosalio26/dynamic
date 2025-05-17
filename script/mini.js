347015f00d52965a560b98b969005414b708ff07
Rosalio26 <rosalioalexandre26@gmail.com>
2025-05-17 09:24:30 +0100

    upload

--- script/mini.js
+++ script/mini.js
@@ -1,3 +1,6 @@
+
+//==============================================================
+//========================
 const btn = document.getElementById('toggleBtn');
 const bloco = document.getElementById('meuBloco');
 const closeBtn = bloco.querySelector('.btn-close');
@@ -16,3 +19,21 @@ closeBtn.addEventListener('click', () => {
   bloco.style.display = 'none';
   btn.style.display = 'block';
   });
+
+
+//========================SCROLL========================
+const scrollBox = document.getElementById('scrollBox');
+    const scrollContent = document.getElementById('scrollContent');
+
+    let isScrolling = true;
+
+    scrollBox.addEventListener('click', () => {
+      if (isScrolling) {
+        scrollContent.style.animationPlayState = 'paused';
+        scrollBox.classList.add('scroll-manual');
+      } else {
+        scrollContent.style.animationPlayState = 'running';
+        scrollBox.classList.remove('scroll-manual');
+      }
+      isScrolling = !isScrolling;
+    });
